4c82d3a3aea370efe1a4cbe0e510e6ca00845615
jessfraser8 <jfraser4@usf.edu>
2024-06-16 21:18:02 -0400

    Implement data loading and DOM manipulation with D3.js.

--- example.js
+++ example.js
@@ -1,2 +1,18 @@
 // U09226075
 // Set up example.js.
+//Create function.
+function main() {
+    d3.csv('../data/purchase_orders.csv').then(
+        function (d) {
+            for (let index = 0; index < d.length; index++) {
+                const element = d[index];
+                var list = document.getElementById("list");
+                var item = document.createElement("li");
+                var text = `${element.customerName} - ${element.orderID}: ${element.purchaseAmount}`;
+                item.appendChild(document.createTextNode(text));
+                list.appendChild(item);
+            }
+        }
+    )
+}
+
